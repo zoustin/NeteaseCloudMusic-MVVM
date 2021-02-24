8fe4473d618a19e2686050c2af0e74b095491b49
zoustin <330286524@qq.com>
2021-02-24 23:57:27 +0800

    添加忽略文件内容

--- app/build/generated/source/dataBinding/trigger/debug/com/netease/music/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.netease.music;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
--- architecture/build/generated/source/dataBinding/trigger/debug/com/kunminx/architecture/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package com.kunminx.architecture;
-
-import androidx.databinding.BindingBuildInfo;
-
-@BindingBuildInfo
-public class DataBindingInfo {}
